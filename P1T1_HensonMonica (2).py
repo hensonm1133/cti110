82fb0f423fff8e5babfd33c234945b877d26fc65
hensonm1133 <36615187+hensonm1133@users.noreply.github.com>
2018-02-18 22:33:36 -0600

    First Submission

---
+++ P1T1_HensonMonica (2).py
@@ -0,0 +1,7 @@
+#P1T1
+#Hello World
+#Monica Henson
+#14 February 2018
+
+print("Hello World")
+
---
+++ P2T1_SalesPrediction_HensonMonica.py
@@ -0,0 +1,16 @@
+#CTI 120-0902
+#P2T1 - Sales Prediction
+#Monica Henson
+#18 February 2018
+#
+#Get the projected total sales.
+total_sales = float (input ('Enter the projected sales: '))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print ('The profit is $', format(profit, ',.2f'))                    
+                    
+
+
